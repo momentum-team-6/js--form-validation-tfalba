086795652845291a3e6b7dbb6a5478fe38aeb89e
Tracy Falba <tfalba@mac.com>
2020-11-14 18:02:04 -0500

    steps 1-4 complete with appropriate RegEx

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 console.log('Add validation!');
 
+//Define all field elements as constants
 
 const form = document.querySelector('#parking-form')
 const daysField = document.querySelector('#days-field')
@@ -9,10 +10,22 @@ const totalField = document.querySelector('#total')
 const submitButton = document.querySelector('#submit-button')
 const carYear = document.querySelector('#car-year')
 const cvv = document.querySelector('#cvv')
+const creditCardNum = document.querySelector('#credit-card')
+
+
+//set RegExp checks for car year >1900 (need to add that into RegExp)
+//sets days to be 1-30
+//sets cvv to be a three digit value
 
 var reCvv = new RegExp("^[0-9]{3}$")
-var reDays = new RegExp("^[0-9]{1,2}$")
-var reCarYear = new RegExp("^[0-9]{4}$")
+// var reDays = new RegExp("^[0-9]{1,2}$")
+
+var reDays = new RegExp("^([1-9]|[12][0-9]|30)$")
+//Looked up above from following tutorial: 
+//https://www.regextutorial.org/regex-for-numbers-and-ranges.php
+
+var reCarYear = new RegExp("^(19[0-9][1-9]|20[0-2][0-9])$")
+var reCarYear = new RegExp("^(19[0-9][1-9]|200[0-9]|201[0-9]|202[01])$")
 
 cvv.addEventListener("input", function(event) {
     if (reCvv.test(cvv.value)===false) {
@@ -24,43 +37,29 @@ cvv.addEventListener("input", function(event) {
 })
 
 carYear.addEventListener("input", function (event) {
-    if (carYear.value<1900 || reCarYear.test(carYear.value)===false) {
-        carYear.setCustomValidity("Year must be greater than 2000.")
+    if (reCarYear.test(carYear.value)===false) {
+        carYear.setCustomValidity("Year must be greater than 1900 and less than 2022.")
     } else {
         carYear.setCustomValidity("")
     }
 })
 
 days.addEventListener("input", function(event) {
-    if (reDays.test(days.value)===false || days.value>30) {
+    if (reDays.test(days.value)===false) {
         days.setCustomValidity("Must be between 1 and 30 days.")
     } else {
         days.setCustomValidity("")
     }
 })
 
-let daysValue = document.querySelector('#days').value
-
 form.addEventListener('submit', function (event) {
   event.preventDefault()
   let total=days.value*5
   console.log(total)
-  totalField.innerHTML = "Total for all of your days is: $" + total 
-//   validate()
+  totalField.innerHTML = "Total for all of your days is: $" + total
+//   form.classList.add('hideme')  -- use this as a way to shift content of the page.
+//    could add a varname.classList.remove('hideme') to whatever was in background
 })
-
-
-// days.addEventListener("input", function (event) {
-//   if (days.validity.typeMismatch) {
-//     days.setCustomValidity("I am expecting a number!");
-//   } else {
-//     days.setCustomValidity("");
-//   }
-//   let total=days.value*5
-
-//   console.log(total)
-//   totalField.innerHTML = "Total for all of your days is: $" + total 
-// });
 
 
 // function validate () {
@@ -68,30 +67,33 @@ form.addEventListener('submit', function (event) {
 //   // validate credit card number
 //   let creditCardNum = document.querySelector('#credit-card').value
 //   validateCardNumber(creditCardNum)
-//   let carYear = document.querySelector('#car-year').value
-// //   let days = document.querySelector('#days-field').value
 // }
 
-// function validateCarYear (number) {
-//     const = 
-// }
+function validateCardNumber (number) {
+  var regex = new RegExp('^[0-9]{16}$')
+  if (!regex.test(number)) return false
+  return luhnCheck(number)
+}
+function luhnCheck (val) {
+  var sum = 0
+  for (var i = 0; i < val.length; i++) {
+    var intVal = parseInt(val.substr(i, 1))
+    if (i % 2 == 0) {
+      intVal *= 2
+      if (intVal > 9) {
+        intVal = 1 + (intVal % 10)
+      }
+    }
+    sum += intVal
+  }
+  return sum % 10 == 0
+}
 
-// function validateCardNumber (number) {
-//   var regex = new RegExp('^[0-9]{16}$')
-//   if (!regex.test(number)) return false
-//   return luhnCheck(number)
-// }
-// function luhnCheck (val) {
-//   var sum = 0
-//   for (var i = 0; i < val.length; i++) {
-//     var intVal = parseInt(val.substr(i, 1))
-//     if (i % 2 == 0) {
-//       intVal *= 2
-//       if (intVal > 9) {
-//         intVal = 1 + (intVal % 10)
-//       }
-//     }
-//     sum += intVal
-//   }
-//   return sum % 10 == 0
-// }
+creditCardNum.addEventListener("input", function(event) {
+    // validateCardNumber(creditCardNum.value)
+    if (validateCardNumber(creditCardNum.value)===false) {
+        creditCardNum.setCustomValidity("Please enter a valid credit card number.")
+    } else {
+        creditCardNum.setCustomValidity("")
+    }
+})
