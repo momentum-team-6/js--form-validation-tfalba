da16dbfc669e374536dde4675a0d76ceba1a5e75
Tracy Falba <tfalba@mac.com>
2020-11-14 19:54:22 -0500

    step 6 complete and future start date for step 7

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const submitButton = document.querySelector('#submit-button')
 const carYear = document.querySelector('#car-year')
 const cvv = document.querySelector('#cvv')
 const creditCardNum = document.querySelector('#credit-card')
+const startDate = document.querySelector('#start-date')
 
 
 //set RegExp checks for car year >1900 (need to add that into RegExp)
@@ -24,8 +25,8 @@ var reDays = new RegExp("^([1-9]|[12][0-9]|30)$")
 //Looked up above from following tutorial: 
 //https://www.regextutorial.org/regex-for-numbers-and-ranges.php
 
-var reCarYear = new RegExp("^(19[0-9][1-9]|20[0-2][0-9])$")
 var reCarYear = new RegExp("^(19[0-9][1-9]|200[0-9]|201[0-9]|202[01])$")
+//above setup will accept any number from 1901-2021
 
 cvv.addEventListener("input", function(event) {
     if (reCvv.test(cvv.value)===false) {
@@ -52,6 +53,9 @@ days.addEventListener("input", function(event) {
     }
 })
 
+
+//This section handles the calculation of total cost for Step 4
+
 form.addEventListener('submit', function (event) {
   event.preventDefault()
   let total=days.value*5
@@ -68,6 +72,8 @@ form.addEventListener('submit', function (event) {
 //   let creditCardNum = document.querySelector('#credit-card').value
 //   validateCardNumber(creditCardNum)
 // }
+
+//This section handles credit card validation for step 6
 
 function validateCardNumber (number) {
   var regex = new RegExp('^[0-9]{16}$')
@@ -97,3 +103,23 @@ creditCardNum.addEventListener("input", function(event) {
         creditCardNum.setCustomValidity("")
     }
 })
+
+//use this as a start on solving step 5 & some of step 7
+
+function validateStartDate (date) {
+    // let myDate= new Date(date)
+    if (new Date(date)<= new Date()) {
+        return false
+    }
+}
+
+startDate.addEventListener("input", function(event) {
+    
+    if (validateStartDate(startDate.value+'T00:00')===false) {
+        startDate.setCustomValidity("Date must begin tomorrow or later.")
+    } else {
+        startDate.setCustomValidity("")
+    }
+})
+
+
